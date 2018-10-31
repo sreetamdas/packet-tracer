f034a433ad6d1e203214ff4858627b7c83816003
Sreetam Das <sreetamdas@gmail.com>
2018-10-31 18:01:13 +0530

    version bump

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDesktop } from "@fortawesome/free-solid-svg-icons";
-import Draggable from "react-draggable";
-import LineTo from "react-lineto";
+import { ReactLines } from "react-lines";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<fieldset className="">
-					<h1>This is App.</h1>
-					{/* <Draggable> */}
-					<div className="E1">
-						<FontAwesomeIcon icon={faDesktop} size="3x" />
-					</div>
-					{/* </Draggable> */}
-					<h1>iuhweifubwiefciuweciuwefcuwbfciwefcwefc</h1>
-					{/* <Draggable> */}
-					<div className="E2">
-						<FontAwesomeIcon icon={faDesktop} size="3x" />
-					</div>
-					{/* </Draggable> */}
-				</fieldset>
-				<LineTo from="E1" to="E2" borderWidth={3} borderColor="red" />
+				<ReactLines />
 			</div>
 		);
 	}
