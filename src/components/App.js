313b49b9e402e3ceae8949105e1f45689038f5f7
Sreetam Das <sreetamdas@gmail.com>
2018-11-15 11:25:26 +0530

    Add react-lines locally

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDesktop } from "@fortawesome/free-solid-svg-icons";
-import ReactLines from "react-lines";
+import ReactLines from "./components/Nodes";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <ReactLines />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<ReactLines />
+			</div>
+		);
+	}
 }
 
 export default App;
